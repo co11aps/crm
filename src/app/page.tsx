b8386d28bcac3746c75d6f11e39f15c43259f7a0
co11aps <oleksandr.fedorov@gmail.com>
2024-10-09 01:59:13 +0800

    clsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import ActiveLabel from './components/ActiveLabel';
-import NotActiveLabel from './components/NotActiveLabel';
+import StatusLabel, { Status } from './components/StatusLabel';
 
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
-      <ActiveLabel>Active</ActiveLabel>
-      <NotActiveLabel>Not Active</NotActiveLabel>
+      <h1 className="text-xl">Home page</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive} disabled>
+        Not Active
+      </StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended} disabled>
+        Suspended
+      </StatusLabel>
     </main>
   );
 }
